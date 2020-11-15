72f2b884d0287b892e5bb4b8fd88029c4f846b34
Kim Nguyen <nnhk23@gmail.com>
2020-11-15 11:53:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "is the champion";
